18395ec7bc8c3882230ffa3399705f901f318434
Patel Jaivik <120545678+Jaivik01092001@users.noreply.github.com>
2024-01-04 10:58:02 +0530

    fix horizontal

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ function App() {
   return (
     <>
       <Header />
-      {/* <Industries /> */}
-      {/* <Choose /> */}
+      <Industries />
+      <Choose />
       {/* <Contact /> */}
       {/* <About /> */}
       {/* <Mission /> */}
