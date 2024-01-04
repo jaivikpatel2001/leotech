344fe25fcc5e07e751c027951552436c62831c0f
Patel Jaivik <120545678+Jaivik01092001@users.noreply.github.com>
2024-01-04 11:10:14 +0530

    fix horizontal

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -5,37 +5,35 @@ import { motion } from "framer-motion";
 const Contact = () => {
   return (
     <>
-      <div className="contactcontainer">
-        <div className="blueheight">
-          <motion.div
-            className="left"
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1 }}
-            transition={{ duration: 2, ease: "easeInOut" }}
-            viewport={{ once: true }}
-          >
-            <div className="heroformtext ">
-              Have a project in mind! Let’s work together.
-            </div>
-            <div className="formdata">
-              <input
-                className="inputfield"
-                type="email"
-                name="email"
-                placeholder="Your email"
-                autoComplete="off"
-              />
-              <button className="submit">Submit</button>
-            </div>
-          </motion.div>
-          <motion.div
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1 }}
-            transition={{ duration: 2, ease: "easeInOut" }}
-            viewport={{ once: true }}
-            className="right"
-          ></motion.div>
-        </div>
+      <div className="blueheight">
+        <motion.div
+          className="left"
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 2, ease: "easeInOut" }}
+          viewport={{ once: true }}
+        >
+          <div className="heroformtext ">
+            Have a project in mind! Let’s work together.
+          </div>
+          <div className="formdata">
+            <input
+              className="inputfield"
+              type="email"
+              name="email"
+              placeholder="Your email"
+              autoComplete="off"
+            />
+            <button className="submit">Submit</button>
+          </div>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 2, ease: "easeInOut" }}
+          viewport={{ once: true }}
+          className="right"
+        ></motion.div>
       </div>
     </>
   );
